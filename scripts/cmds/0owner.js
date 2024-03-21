9fb49d9bd7f7435c10b9e9b80ae110507b30e9b0
Raj <159320744+Nobita-789@users.noreply.github.com>
2024-03-21 17:44:27 +0530

    Update 0owner.js

--- scripts/cmds/0owner.js
+++ scripts/cmds/0owner.js
@@ -24,7 +24,7 @@ config: {
       nick: 'Yazky'
     };
 
-    const bold = 'https://i.imgur.com/DDO686J.mp4'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
+    const bold = 'https://i.postimg.cc/cJbWNjt7/1708430500798.jpg'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
 
     const tmpFolderPath = path.join(__dirname, 'tmp');
 
